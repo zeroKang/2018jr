47ed270676edcf95946210ea6ca42a2ecb28b213
zzz <zzz@DESKTOP-3OT99P7>
2018-04-27 16:20:03 +0900

    Map Dynamic SQL

--- dySQL/src/main/java/org/zerock/search/SearchMapper.java
+++ dySQL/src/main/java/org/zerock/search/SearchMapper.java
@@ -1,6 +1,19 @@
 package org.zerock.search;
 
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Param;
+
 public interface SearchMapper {
 
 	public String getTime();
+	
+	public List<String> search1(
+			@Param("type")String type, 
+			@Param("keyword") String keyword);
+
+	public List<String> search2(Map<String, String> paramMap);
 }
+
+
--- dySQL/src/test/java/org/zerock/controller/SearchMapperTests.java
+++ dySQL/src/test/java/org/zerock/controller/SearchMapperTests.java
@@ -1,5 +1,8 @@
 package org.zerock.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +19,26 @@ public class SearchMapperTests {
 
 	@Autowired
 	private SearchMapper mapper;
+	
+	@Test
+	public void test1() {
+		
+		log.info(mapper.search1("c", "테스트"));
+	}
+	
+	@Test
+	public void test2() {
+		
+		Map<String, String> map = new HashMap<String, String>();
+		
+		map.put("t", "테스트");
+		map.put("c", "테스트");
+		map.put("w", "테스트");
+		
+
+		
+		log.info(mapper.search2(map));
+	}
 	
 	
 	@Test
